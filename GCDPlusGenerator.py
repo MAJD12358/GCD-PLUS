9456633e54447f6f3cd6c5428767e89f5b74f0ee
GCD+ <127302048+MAJD12358@users.noreply.github.com>
2024-02-22 10:22:40 +0200

    Update GCDPlusGenerator.py

--- GCDPlusGenerator.py
+++ GCDPlusGenerator.py
@@ -1,39 +1,24 @@
-from antlr4 import *
-from GCDplusLexer import GCDplusLexer
-from GCDplusParser import GCDplusParser
+import json
 
-# Extend the parser and override the default error handler
-class MyErrorListener(ParseTreeListener):
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        raise Exception(f"Syntax Error at line {line}:{column} - {msg}")
+def generate_gcdplus_from_json(json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
 
-def main():
-    # Read input GCD+ code from a file
-    with open('input.gcd+', 'r') as file:
-        gcdPlusCode = file.read()
+    gcdplus_code = f"# {data['language']} Configuration\n\n"
+    gcdplus_code += f"Language#{data['language']}#{data['version']}\n"
+    gcdplus_code += f"Author#{data['author']}\n\n"
+    
+    for feature, value in data['features'].items():
+        gcdplus_code += f"{feature}#true\n" if value else f"{feature}#false\n"
 
-    # Create an ANTLR input stream from the GCD+ code
-    stream = InputStream(gcdPlusCode)
+    gcdplus_code += "\nDefaults\n"
+    for key, value in data['defaults'].items():
+        gcdplus_code += f"  {key}#{value}\n"
 
-    # Create a lexer that reads from the input stream
-    lexer = GCDplusLexer(stream)
+    return gcdplus_code
 
-    # Create a buffer of tokens pulled from the lexer
-    tokenStream = CommonTokenStream(lexer)
-
-    # Create a parser that reads from the token stream
-    parser = GCDplusParser(tokenStream)
-
-    # Attach custom error listener
-    parser.removeErrorListeners()
-    parser.addErrorListener(MyErrorListener())
-
-    # Start parsing from the program rule
-    tree = parser.program()
-
-    # Print the parse tree
-    print(tree.toStringTree(recog=parser))
-
-if __name__ == '__main__':
-    main()
-  
+# Usage example:
+json_file_path = 'gcdplus-config.json'
+generated_code = generate_gcdplus_from_json(json_file_path)
+print(generated_code)
+        
